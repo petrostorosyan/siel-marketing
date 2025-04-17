127bd58cd52be144d4cffef451967cd0f3e4cc2d
petrostorosyan <petrostorosyan23@gmail.com>
2025-04-17 10:32:02 +0400

    continue building package

--- src/components/packages/package/package.jsx
+++ src/components/packages/package/package.jsx
@@ -4,12 +4,23 @@ import styles from "./package.module.scss";
 const Package = () => {
   return (
     <div className={styles.packageWrapper}>
-        <div className={styles.titleAndImage}>
-            <h4 className={styles.packageTitle}>Core Package</h4>
-            <Image src={"/images/packages/bag.svg"} width={84} height={77} alt="bag"></Image>
-        </div>
+      <div className={styles.titleAndImage}>
+        <h4 className={styles.packageTitle}>Core Package</h4>
+        <Image
+          src={"/images/packages/bag.svg"}
+          width={84}
+          height={77}
+          alt="bag"
+        ></Image>
+      </div>
+      <p className={styles.packageDescription}>
+        This package offers essential branding, content, marketing, and
+        AI-driven solutions, ideal for businesses looking to build an online
+        presence with targeted strategies.
+      </p>
+      <div className={styles.paymentBlock}></div>
     </div>
-  )
-}
+  );
+};
 
 export default Package;
