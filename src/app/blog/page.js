698b4a34f394da75598051e84bc2ec7a5860656f
Petros <petrostorosyan23@gmail.com>
2025-05-06 09:05:36 +0400

    continue build blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -21,7 +21,24 @@ export default function LikeButton() {
             behind color choices in marketing design and how to use them
             strategically to inspire action.
           </p>
-          <Link href={"/"} className={styles.readingButton}>Continue Reading</Link>
+          <Link href={"/"} className={styles.readingButton}>
+            Continue Reading
+          </Link>
+        </div>
+        <div className={styles.mobileBlogInfo}>
+          <h4 className={styles.mobileInfoTitle}>
+            Did you know the colors you choose can influence your audience’s
+            emotions and decisions?
+          </h4>
+          <p className={styles.mobileInfoDescription}>
+            In this blog, we’ll explore the psychology behind color choices in
+            marketing design and how to use them strategically to inspire
+            action.
+          </p>
+          <div className={styles.readMoreBox}>
+            <p className={styles.uploadTime}>10h ago</p>
+            <button className={styles.readMoreBtn}>Read More</button>
+          </div>
         </div>
       </div>
     </div>
