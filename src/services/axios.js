8ef6ae0ba28693d43eba66d648a251b3e2f097d8
petrostorosyan <petrostorosyan23@gmail.com>
2025-05-06 11:17:16 +0400

    create blogs data

--- src/services/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const Instance = axios.create({
-    baseURL: "https://base-api-dev.azurewebsites.net/base/"
-    // baseURL: process.env.NODE_ENV==='development'?'https://base-api-dev.azurewebsites.net/base/':'https://api.itbase.am/base/'
-})
-
-
-export default Instance;
---
+++ src/services/blogs/blogsData.js
@@ -0,0 +1,35 @@
+export const blogsData = [
+  {
+    id: 1,
+    uploadDate: "14 Feb 2025",
+    imageSource: "/images/blog/digital-marketing-future-small.webp",
+    title: "The Future of Digital Marketing: What’s Changing in 2025?",
+    description:
+      "The digital marketing landscape is evolving faster than ever, and if you’re not keeping up, you’re falling behind. As we step into 2025, businesses need to adapt to the latest trends.",
+  },
+  {
+    id: 2,
+    uploadDate: "14 Feb 2025",
+    imageSource: "/images/blog/ai-chatbot-small.webp",
+    title: "AI Chatbots & Marketing Automation: How to Engage Customers 24/7",
+    description:
+      "Imagine never missing a potential customer inquiry, no matter the time of day. AI chatbots and marketing automation are making this a reality.",
+  },
+  {
+    id: 3,
+    uploadDate: "14 Feb 2025",
+    imageSource: "/images/blog/content-marketing-small.webp",
+    title:
+      "Content Marketing in the AI Era: Is Human-Created Content Still Relevant?",
+    description:
+      "AI-generated content is everywhere, from social media captions to full-length blog posts. But does this mean human storytelling is becoming obsolete?",
+  },
+  {
+    id: 4,
+    uploadDate: "14 Feb 2025",
+    imageSource: "/images/blog/blog-primary-pic.webp",
+    title: "Colors You Choose for Your Brand",
+    description:
+      "Did you know the colors you choose can influence your audience’s emotions and decisions?",
+  },
+];
