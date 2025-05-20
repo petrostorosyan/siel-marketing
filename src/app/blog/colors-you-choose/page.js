b37dee241da90435bac0e9f49efdb723cffc8165
Petros <petrostorosyan23@gmail.com>
2025-05-20 09:05:49 +0400

    finish colors blog page

--- src/app/blog/colors-you-choose/page.js
+++ src/app/blog/colors-you-choose/page.js
@@ -8,13 +8,13 @@ export default async function Page({ params: { id } }) {
   return (
     <div className={styles.articleContainer}>
       <div className={styles.headerBackground}></div>
-      {/* <div className={styles.contentBlock}>
+      <div className={styles.contentBlock}>
         <div className={styles.contentBody}></div>
         <div className={styles.article}>
           <ArticleContent />
         </div>
         <div className={styles.bottomBackground}></div>
-      </div> */}
+      </div>
       <div className={styles.changeBlogButtons}>
         <ChangeBlogButtons
           prevPageName="Content Marketing in the AI Era"
--- src/components/shared/changeBlogButtons/changeBlogButtons.jsx
+++ src/components/shared/changeBlogButtons/changeBlogButtons.jsx
@@ -3,28 +3,42 @@ import styles from "./changeBlogButtons.module.scss";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import SocialIcons from "./socialIcons/socialIcons";
 
-const ChangeBlogButtons = ({ prevPageName, prevPageLink, nextPageName, nextPageLink }) => {
+const ChangeBlogButtons = ({
+  prevPageName,
+  prevPageLink,
+  nextPageName,
+  nextPageLink,
+}) => {
   return (
     <div className={styles.changeBlogContainer}>
-      <div className={styles.changeBlogWrapper}>
-        <Link href={prevPageLink} className={styles.pageLink}>
-          <button className={styles.button}>
-            <KeyboardBackspaceIcon sx={{ marginRight: "15px" }} />
-            Previous
-          </button>
-          <p className={styles.pageName}>{prevPageName}</p>
-        </Link>
+      <div className={styles.changeBlogContent}>
+        <div className={styles.changeBlogWrapper}>
+          <Link href={prevPageLink} className={styles.pageLink}>
+            <button className={styles.button}>
+              <KeyboardBackspaceIcon sx={{ marginRight: "15px" }} />
+              Previous
+            </button>
+            <p className={styles.pageName}>{prevPageName}</p>
+          </Link>
 
-        <div className={styles.socialIcons}><SocialIcons /></div>
-        <Link href={nextPageLink} className={styles.pageLink}>
-          <button className={styles.button}>
-            Next
-            <KeyboardBackspaceIcon
-              sx={{ transform: "rotate(180deg)", marginLeft: "15px" }}
-            />
-          </button>
-          <p className={styles.pageName}>{nextPageName}</p>
-        </Link>
+          <div className={styles.socialIcons}>
+            <SocialIcons />
+          </div>
+          <Link href={nextPageLink} className={styles.pageLink}>
+            <button className={styles.button}>
+              Next
+              <KeyboardBackspaceIcon
+                sx={{ transform: "rotate(180deg)", marginLeft: "15px" }}
+              />
+            </button>
+            <p className={styles.pageName}>{nextPageName}</p>
+          </Link>
+        </div>
+        <div className={styles.mobileIconsBox}>
+          <div className={styles.mobileSocialIcons}>
+            <SocialIcons />
+          </div>
+        </div>
       </div>
     </div>
   );
